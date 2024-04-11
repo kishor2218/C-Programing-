d8f81b28157c300d4018972232b48491cf36b1cb
kishor2218 <144766244+kishor2218@users.noreply.github.com>
2024-04-11 17:54:48 +0530

    Create SumOfNaturalNumber.c

---
+++ SumOfNaturalNumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    for(int i=0;i<num;i++){
+        sum+=i;
+    }
+    printf("Sum:%d",sum);
+    return 0;
+}
