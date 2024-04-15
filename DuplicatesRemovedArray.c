883721446bfdb947b6f29378153227ffbacbb912
kishor2218 <144766244+kishor2218@users.noreply.github.com>
2024-04-15 14:21:30 +0530

    Create DuplicatesRemovedArray.c

---
+++ DuplicatesRemovedArray.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n ,count=1;
+    printf("enter the array size:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter array elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int b[n] ,c[n];
+    for(int i=0;i<n;i++){
+        if(b[i]==0){
+        int count=1;
+        for(int j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                count++;
+                b[j]=1;
+            }
+        }
+       
+       printf("%d",a[i]);
+       c[i]=a[i];
+        }
+        
+    }
+    return 0;
+}
